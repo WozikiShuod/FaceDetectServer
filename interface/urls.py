d8d82b8bad4442149b79b78c7182c32b95097108
WozikiShuod <WozikiShuod@users.noreply.github.com>
2019-07-15 21:37:44 +0800

    上传图片添加

--- interface/urls.py
+++ interface/urls.py
@@ -3,7 +3,8 @@
 # @time : 16:53   2019/7/14
 # @filename : urls.py
 from django.urls import path
-from .views import index
+from .views import index,pictures_save
 urlpatterns=[
+    path('pictures_save/', pictures_save, name='图片上传'),
     path('qrcode/',index,name='二维码'),
 ]
--- interface/views.py
+++ interface/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.conf import settings
 import os
 # Create your views here.
 def index(request):
@@ -10,5 +11,29 @@ def index(request):
     context['main']=pics
     return render(request, 'interface/qrcode.html', context=context)
 
+
 if __name__ == '__main__':
     print(os.curdir)
+
+
+def pictures_save(request):
+    obj = dict();
+    if request.method == 'POST':
+        student_name = str(request.POST.get('username'))
+        files = request.FILES.getlist('photo')
+        if not files:
+            obj['error'] = '没有图片上传'
+        else:
+            dirs = settings.MEDIA_ROOT+student_name
+            folder = os.path.exists(dirs)
+            if not folder:
+                os.makedirs(dirs)
+            try:
+                for file in files:
+                    img_name = file.name
+                    test = file.file.read()
+                    with open(f'{dirs}'+'\\'+img_name, 'wb') as f:
+                        f.write(test)
+            except Exception as e:
+                obj['error'] = e
+    return render(request, 'interface/Pictures_Save.html')
